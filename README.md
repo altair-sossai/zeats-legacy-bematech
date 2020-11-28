85025e477f3164d998583fd40c9cbf7c0b775ff2
Altair Sossai <altair_sossai@outlook.com>
2020-11-28 11:19:00 -0300

    Inicio do projeto

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+
+![Zeats](https://zeatsbalancaautomatica.blob.core.windows.net/icons/nuget.png)
+
+</div>
+
+# zeats-legacy-bematech
+
+Extensions to solve common problems when using Bematech printers
+
+[![Build Status](https://dev.azure.com/zeats/Legacy/_apis/build/status/zeats-legacy-bematech?branchName=master)](https://dev.azure.com/zeats/Legacy/_build/latest?definitionId=17&branchName=master)
+[![NuGet](https://img.shields.io/nuget/v/Zeats.Legacy.Bematech.svg)](https://www.nuget.org/packages/Zeats.Legacy.Bematech)
+
+## Installation
+
+```PM>
+Install-Package Zeats.Legacy.Bematech
+```
---
+++ Zeats.Legacy.Bematech/Services/BematechPrintService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Zeats.Bematech.Impressoras;
+using Zeats.Legacy.PlainTextTable.Print.Enums;
+using Zeats.Legacy.PlainTextTable.Print.Print;
+
+namespace Zeats.Legacy.Bematech.Services
+{
+    public class BematechPrintService : IPrintService
+    {
+        private static readonly object Lock = new object();
+
+        public void Print(PrintCollection printCollection)
+        {
+            lock (Lock)
+            {
+                var retorno = MP2032.IniciaPorta(printCollection.Options.PortName);
+                if (retorno == 0)
+                    throw new Exception("Não foi possível estabelecer comunicação com a impressora");
+
+                foreach (var printItem in printCollection)
+                {
+                    retorno = printItem.FontType == FontType.Text ? PrintText(printItem) : PrintBarCode(printItem);
+
+                    if (retorno == 0)
+                        throw new Exception("Ocorreu um erro ao enviar um comando de impressão para a impressora");
+                }
+
+                retorno = MP2032.FechaPorta();
+                if (retorno == 0)
+                    throw new Exception("Não foi possível estabelecer comunicação com a impressora");
+            }
+        }
+
+        public void Cut(string portName, CutType cutType = CutType.Full)
+        {
+            lock (Lock)
+            {
+                MP2032.IniciaPorta(portName);
+                MP2032.AcionaGuilhotina(cutType == CutType.Partial ? 0 : 1);
+                MP2032.FechaPorta();
+            }
+        }
+
+        private static int PrintBarCode(PrintItem printItem)
+        {
+            MP2032.ConfiguraCodigoBarras(100, 2, 2, 1, 20);
+            var retorno = MP2032.ImprimeCodigoBarrasEAN13(printItem.Content);
+            return retorno;
+        }
+
+        private static int PrintText(PrintItem printItem)
+        {
+            var tipoLetra = printItem.FontSize == FontSize.Small ? 1
+                : printItem.FontSize == FontSize.Normal ? 2
+                : printItem.FontSize == FontSize.Large ? 3
+                : 2;
+
+            var italico = printItem.Italic ? 1 : 0;
+            var sublinhado = printItem.Underline ? 1 : 0;
+            var expandido = printItem.FontSize == FontSize.Large ? 1 : 0;
+            var enfatizado = printItem.Bold ? 1 : 0;
+
+            var retorno = MP2032.FormataTX(printItem.Content, tipoLetra, italico, sublinhado, expandido, enfatizado);
+            return retorno;
+        }
+    }
+}
